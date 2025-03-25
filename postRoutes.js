e30521886e3c8d4f163bb972b8cbaae6c13d5878
julianna ♡ <amanda_delcarmen@dlsu.edu.ph>
2025-03-26 02:11:15 +0800

    Delete postRoutes.js

--- postRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getAllPosts, createPost, votePost } = require("../controllers/postController");
-
-const router = express.Router();
-router.get("/", getAllPosts);
-router.post("/create", createPost);
-router.post("/:id/vote", votePost);
-
-module.exports = router;
