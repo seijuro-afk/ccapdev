44c40d1eed0c111375bd3ab8aca3eb5d43af7357
julianna ♡ <amanda_delcarmen@dlsu.edu.ph>
2025-04-08 15:21:01 +0800

    updated implementation of comments

--- models/commentModel.js
+++ models/commentModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const CommentSchema = new mongoose.Schema({
     postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
-    author: { type: String, required: true },
+    author: { type: String, default: "Anonymous" },
     content: { type: String, required: true },
     created_at: { type: Date, default: Date.now }
 });
