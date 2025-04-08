b55aa95c5b70c2d52e8448cec51f669780958671
julianna ♡ <amanda_delcarmen@dlsu.edu.ph>
2025-04-08 15:19:39 +0800

    Delete commentRoutes.js

--- commentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { addComment, getAllComments } = require("../controllers/commentController");
-
-const router = express.Router();
-router.post("/add", addComment);
-router.get("/", getCommentsByPost);
-
-module.exports = router;
